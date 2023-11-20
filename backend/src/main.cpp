695a455e47e717313a97501c43bf35e6282759f6
nyabla <16510465+nyabla@users.noreply.github.com>
2023-11-20 21:59:29 +0000

    main: sketch it out

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -3,3 +3,39 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
+
+#include "oatpp/network/Server.hpp"
+
+#include "Components.hpp"
+#include "controller/TorrentController.hpp"
+
+void run() {
+    // register components
+    Components components;
+    
+    OATPP_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, router);
+    router->addController(TorrentController::createShared());
+
+    OATPP_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, connectionHandler);
+    OATPP_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, connectionProvider);
+    
+    // create server
+    oatpp::network::Server server(connectionProvider, connectionHandler);
+
+    OATPP_LOGI("main", "server running on port %s", connectionProvider->getProperty("port").getData());
+
+    // run server run!
+    server.run();
+}
+
+int main() {
+    // initialise environment
+    oatpp::base::Environment::init();
+
+    run();
+
+    // tidy up environment
+    oatpp::base::Environment::destroy();
+
+    return 0;
+}
