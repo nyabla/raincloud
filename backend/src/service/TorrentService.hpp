7d4439e4e81208497a8d41131aaeb125d3339e91
nyabla <16510465+nyabla@users.noreply.github.com>
2023-11-21 14:16:07 +0000

    TorrentService: use new Result

--- backend/src/service/TorrentService.hpp
+++ backend/src/service/TorrentService.hpp
@@ -13,9 +13,11 @@
 #include <libtorrent/torrent_flags.hpp>
 #include <libtorrent/torrent_handle.hpp>
 
+#include "util.hpp"
+
 class TorrentService {
 public:
-    class AddTorrent : public oatpp::async::Coroutine<AddTorrent> {
+    class AddTorrent : public oatpp::async::CoroutineWithResult<AddTorrent, util::Result<std::string, std::string>> {
     private:
         OATPP_COMPONENT(std::shared_ptr<lt::session>, m_session);
 
@@ -25,10 +27,16 @@ public:
         AddTorrent(const std::string magnet) : m_magnet(magnet) {}
 
         Action act() override {
-            auto torrentParams = lt::parse_magnet_uri(m_magnet);
-            torrentParams.flags = lt::torrent_flags::default_dont_download;
+            try {
+                auto torrentParams = lt::parse_magnet_uri(m_magnet);
+                torrentParams.flags = lt::torrent_flags::default_dont_download;
 
-            m_torrentHandle = m_session->add_torrent(torrentParams);
+                m_torrentHandle = m_session->add_torrent(torrentParams);
+            } catch (std::exception e) {
+                _return(
+                    util::Result<std::string, std::string>::Err("oops")
+                );
+            }
 
             return yieldTo(&AddTorrent::checkReady);
         }
@@ -38,12 +46,9 @@ public:
                 return waitRepeat(std::chrono::milliseconds(50));
             }
 
-            return yieldTo(&AddTorrent::ready);
-        }
-
-        Action ready() {
-            /* TODO */
-            return finish();
+            return _return(
+                util::Result<std::string, std::string>::Ok(m_torrentHandle.info_hash().to_string())
+            );
         }
     };
 };
