b45c478cf44a718a568514206fcacc63d04f81f4
nyabla <16510465+nyabla@users.noreply.github.com>
2023-11-23 15:20:07 +0000

    Requests: custom Expector

--- webui/src/Requests.elm
+++ webui/src/Requests.elm
@@ -4,6 +4,7 @@ import Http
 import Codec
 import Config exposing (config)
 import Url.Builder exposing (crossOrigin)
+import Json.Decode exposing (Decoder)
 
 type RequestStatus a
   = None
@@ -11,10 +12,18 @@ type RequestStatus a
   | Error String
   | Result a
 
-type alias AddTorrentResult = Result Http.Error Codec.TorrentAddResponse
+type RequestError
+  = Timeout
+  | NetworkError
+  | BadStatus Int String
+  | BadBody String
+
+type alias RequestResult response = Result RequestError response
+
+type alias AddTorrentResult = RequestResult Codec.TorrentAddResponse
 
 addTorrent : String -> (AddTorrentResult -> msg) -> Cmd msg
-addTorrent magnet message =
+addTorrent magnet toMsg =
   Http.request
     { method = "POST"
     , headers = []
@@ -22,12 +31,12 @@ addTorrent magnet message =
     , body = Http.jsonBody
       <| Codec.torrentAddRequestEncoder
       <| Codec.TorrentAddRequest magnet
-    , expect = Http.expectJson message Codec.torrentAddResponseDecoder
+    , expect = expectApiResponse toMsg Codec.torrentAddResponseDecoder
     , timeout = Just (30 * 1000)
     , tracker = Nothing
     }
 
-type alias GetFilesListResult = Result Http.Error Codec.TorrentListFilesResponse
+type alias GetFilesListResult = RequestResult Codec.TorrentListFilesResponse
 
 getTorrentFiles : String -> (GetFilesListResult -> msg) -> Cmd msg
 getTorrentFiles infoHash message =
@@ -36,7 +45,36 @@ getTorrentFiles infoHash message =
     , headers = []
     , url = crossOrigin config.apiBase [ "torrent", infoHash, "files" ] []
     , body = Http.emptyBody
-    , expect = Http.expectJson message Codec.torrentListFilesResponseDecoder
+    , expect = expectApiResponse message Codec.torrentListFilesResponseDecoder
     , timeout = Just (30 * 1000)
     , tracker = Nothing
     }
+
+
+-- EXPECT
+
+expectApiResponse : (RequestResult a -> msg) -> Decoder a -> Http.Expect msg
+expectApiResponse toMsg decoder =
+  Http.expectStringResponse toMsg <|
+    \response ->
+      case response of
+        Http.BadUrl_ _ ->
+          -- pretending this doesn't exist sorry
+          Err NetworkError
+
+        Http.Timeout_ ->
+          Err Timeout
+
+        Http.NetworkError_ ->
+          Err NetworkError
+
+        Http.BadStatus_ metadata body ->
+          Err (BadStatus metadata.statusCode body)
+
+        Http.GoodStatus_ _ body ->
+          case Json.Decode.decodeString decoder body of
+            Ok value ->
+              Ok value
+
+            Err err ->
+              Err (BadBody (Json.Decode.errorToString err))
